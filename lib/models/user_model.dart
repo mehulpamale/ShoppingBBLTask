88d447a01357acd2bf5499c9dc8e0834aada3d8e
Mehul Pamale(mehul.pamale) <mehul.pamale@infosys.com>
2021-11-24 13:48:04 +0530

    added usermodel

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,4 +1,39 @@
+// To parse this JSON data, do
+//
+//     final userModel = userModelFromJson(jsonString);
+
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
+
+String userModelToJson(UserModel data) => json.encode(data.toJson());
+
+class UserModel {
+  UserModel({
+    required this.number,
+    required this.cart,
+  });
+
+  final int number;
+  final Map<int, int> cart;
+
+  factory UserModel.fromDocumentSnapshot(DocumentSnapshot snap) => UserModel(
+        number: snap["number"],
+        cart: snap["cart"],
+      );
+
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+        number: json["number"],
+        cart: json["cart"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "number": number,
+        "cart": cart,
+      };
+}
 
 void main() {
   userTest();
